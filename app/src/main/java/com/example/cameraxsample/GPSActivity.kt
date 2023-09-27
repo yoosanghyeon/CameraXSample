fc93fbd1df7e5513851f86c5af599df7aa9a993b
YooSangHyeone <yoosanghyeon1@gmail.comm>
2023-09-27 09:48:29 +0900

    Android Naver Map working

--- app/src/main/java/com/example/cameraxsample/GPSActivity.kt
+++ app/src/main/java/com/example/cameraxsample/GPSActivity.kt
@@ -2,9 +2,8 @@ package com.example.cameraxsample
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.graphics.PointF
-import android.location.Location
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,9 +12,9 @@ import com.example.cameraxsample.databinding.ActivityGpsactivityBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
@@ -27,59 +26,55 @@ class GPSActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private lateinit var viewBinding: ActivityGpsactivityBinding
-    private lateinit var mapView: MapView
+
+    private lateinit var naverMap: NaverMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityGpsactivityBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        mapView = findViewById(R.id.map_view)
-        mapView.onCreate(savedInstanceState)
-    }
 
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
-    }
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
 
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
+        mapFragment.getMapAsync(this)
 
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            fusedLocationClient.lastLocation.addOnSuccessListener {
+                val cameraUpdate = CameraUpdate.scrollTo(LatLng(it.latitude, it.longitude))
+                    .animate(CameraAnimation.Easing)
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
-    }
+                naverMap.moveCamera(cameraUpdate)
+            }
 
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
     }
 
     override fun onMapReady(p0: NaverMap) {
-//        val cameraUpdate = CameraUpdate.scrollTo(LatLng(36.5666102, 126.9783881))
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5666102, 126.9783881))
-            .pivot(PointF(0.5f, 0.8f))
+        Log.e(TAG, "OnmapReady")
+        naverMap = p0
 
-        p0.moveCamera(cameraUpdate)
-        p0.extent = LatLngBounds(LatLng(31.43, 122.37), LatLng(44.35, 132.0))
+//        Handler().postDelayed({
+//            val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.4816836, 126.8848364))
+//                .animate(CameraAnimation.Easing)
+//
+//            naverMap.moveCamera(cameraUpdate)
+//        }, 100)
     }
+
 
 }
